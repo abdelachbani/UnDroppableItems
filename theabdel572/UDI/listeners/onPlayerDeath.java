62f201118b91c7b95b95a449b26f0c3a33c6c3a7
theabdel572 <abdelkarimachbaniabid572@gmail.com>
2021-04-24 11:45:17 +0200

    Added PlayerDeathEvent.

---
+++ theabdel572/UDI/listeners/onPlayerDeath.java
@@ -0,0 +1,49 @@
+package theabdel572.UDI.listeners;
+
+import java.util.List;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+import theabdel572.UDI.UDI;
+
+public class onPlayerDeath implements Listener{
+	private final UDI plugin;
+	public onPlayerDeath(UDI plugin) {
+		this.plugin = plugin;
+	}
+	
+	@EventHandler
+	public void onDeath(PlayerDeathEvent e) {
+		Player player = e.getEntity();
+		if(player.hasPermission("udi.exclude")) {
+			return;
+		}else {
+			List<ItemStack> drops = e.getDrops();
+			FileConfiguration config = plugin.getConfig();
+			
+			for(int i=0; i<drops.size(); i++) {
+				ItemStack item = drops.get(i);
+				String material = item.getType().toString();
+				String name = item.getItemMeta().getDisplayName();
+				List<String> lore = item.getItemMeta().getLore();
+				
+				if(config.getStringList("Items."+material+"."+name+".lore").equals(lore)) {
+					drops.remove(i);
+				}else if(config.contains("Items."+material+"."+name+".name")) {
+					drops.remove(i);
+				}else if(config.getStringList("Items."+material+".lore").equals(lore)) {
+					drops.remove(i);
+				}else if(config.getBoolean("Items."+material+".undroppable") == true) {
+					drops.remove(i);
+				}else {
+					return;
+				}
+			}
+		}
+	}
+}
