4b21f727c9b79de0a8ef584e56b7e70ee404b2e5
theabdel572 <abdelkarimachbaniabid572@gmail.com>
2021-04-25 10:50:51 +0200

    Didn't work... fix?

--- theabdel572/UDI/listeners/onPlayerDeath.java
+++ theabdel572/UDI/listeners/onPlayerDeath.java
@@ -11,36 +11,36 @@ import org.bukkit.inventory.ItemStack;
 
 import theabdel572.UDI.UDI;
 
-public class onPlayerDeath implements Listener{
+public class onPlayerDeath implements Listener {
 	private final UDI plugin;
+
 	public onPlayerDeath(UDI plugin) {
 		this.plugin = plugin;
 	}
-	
+
 	@EventHandler
 	public void onDeath(PlayerDeathEvent e) {
 		Player player = e.getEntity();
-		if(player.hasPermission("udi.exclude")) {
+		if (player.hasPermission("udi.exclude")) {
 			return;
-		}else {
+		} else {
 			List<ItemStack> drops = e.getDrops();
 			FileConfiguration config = plugin.getConfig();
-			
-			for(int i=0; i<drops.size(); i++) {
+			for (int i = 0; i < drops.size(); i++) {
 				ItemStack item = drops.get(i);
 				String material = item.getType().toString();
 				String name = item.getItemMeta().getDisplayName();
 				List<String> lore = item.getItemMeta().getLore();
-				
-				if(config.getStringList("Items."+material+"."+name+".lore").equals(lore)) {
-					drops.remove(i);
-				}else if(config.contains("Items."+material+"."+name+".name")) {
-					drops.remove(i);
-				}else if(config.getStringList("Items."+material+".lore").equals(lore)) {
-					drops.remove(i);
-				}else if(config.getBoolean("Items."+material+".undroppable") == true) {
-					drops.remove(i);
-				}else {
+
+				if (config.getStringList("Items." + material + "." + name + ".lore").equals(lore)) {
+					drops.remove(item);
+				} else if (config.contains("Items." + material + "." + name + ".name")) {
+					drops.remove(item);
+				} else if (config.getStringList("Items." + material + ".lore").equals(lore)) {
+					drops.remove(item);
+				} else if (config.getBoolean("Items." + material + ".undroppable") == true) {
+					drops.remove(item);
+				} else {
 					return;
 				}
 			}
